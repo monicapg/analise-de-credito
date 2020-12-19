70586ed1f33b2f4d0602af337f7a639d4af16b06
Rui Braz <rui.raz.correia@gmail.com>
2020-12-19 17:03:03 -0300

    Commit inicial.

---
+++ src/main/java/br/com/gomidesmp/analisedecredito/AnaliseDeCreditoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gomidesmp.analisedecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDeCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseDeCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gomidesmp/analisedecredito/AnaliseDeCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gomidesmp.analisedecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseDeCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
